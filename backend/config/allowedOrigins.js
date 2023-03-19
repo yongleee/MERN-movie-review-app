d85a7d6ffc7a2d24b0948a7ab785e8c0025ce49c
yong le lee <leeyl9797@gmail.com>
2023-03-19 21:59:51 +0800

    added base url in backend

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "https://www.kinopicks.com",
   "http://localhost:4000",
   "http://localhost:3000",
+  "https://kinopicks.onrender.com",
 ];
 
 module.exports = allowedOrigins;
