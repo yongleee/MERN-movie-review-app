82ed32a4b38acb41439515d5107d6291a9b8aec8
yong le lee <leeyl9797@gmail.com>
2023-03-20 10:33:27 +0800

    set up express server to serve static files from backend dir

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,17 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 app.use((req, res, next) => {
