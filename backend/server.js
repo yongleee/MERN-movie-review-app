a50fc5f1eff400128905f8ebd6eee122bef2c8b9
yong le lee <leeyl9797@gmail.com>
2023-03-20 10:18:20 +0800

    minor edits for consistency

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,10 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "frontend", "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 app.use((req, res, next) => {
