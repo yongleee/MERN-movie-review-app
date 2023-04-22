a1bf2c1e9c4178303dccd386b4a98a50ae28b039
yong le lee <leeyl9797@gmail.com>
2023-04-22 22:58:27 +0800

    minor changes in user review cards component

--- frontend/src/components/UserReviewCards.js
+++ frontend/src/components/UserReviewCards.js
@@ -57,7 +57,7 @@ export default function UserReviewCards({
   // TODO: styling: work on double confirm delete for user using modal
   return (
     <li>
-      <div className="py-4 px-5 w-11/12 border border-neutral-500 bg-neutral-600/50 rounded drop-shadow-lg my-3">
+      <div className="py-4 px-5 md:w-11/12 w-full border border-neutral-500 bg-neutral-600/50 rounded drop-shadow-lg my-3">
         <div className="flex">
           <p className="text-neutral-400">
             <span className="text-neutral-200 hover:text-neutral-200/75">
