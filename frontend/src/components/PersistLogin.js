a4d5709d5d80d82d54cba8b47de44f839cb16f3f
yong le lee <leeyl9797@gmail.com>
2023-09-10 23:11:09 +0800

    added additional loading message

--- frontend/src/components/PersistLogin.js
+++ frontend/src/components/PersistLogin.js
@@ -4,41 +4,47 @@ import { useAuthContext } from "../hooks/useAuthContext";
 import { useRefreshToken } from "../hooks/useRefreshToken";
 
 const PersistLogin = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const refresh = useRefreshToken();
-  const { auth } = useAuthContext();
-  // console.log(auth);
+	const [isLoading, setIsLoading] = useState(true);
+	const refresh = useRefreshToken();
+	const { auth } = useAuthContext();
+	// console.log(auth);
 
-  useEffect(() => {
-    let isMounted = true;
+	useEffect(() => {
+		let isMounted = true;
 
-    const verifyRefreshToken = async () => {
-      try {
-        await refresh();
-      } catch (err) {
-        console.error(err);
-      } finally {
-        isMounted && setIsLoading(false);
-      }
-    };
+		const verifyRefreshToken = async () => {
+			try {
+				await refresh();
+			} catch (err) {
+				console.error(err);
+			} finally {
+				isMounted && setIsLoading(false);
+			}
+		};
 
-    !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+		!auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
 
-    return () => (isMounted = false);
-    // eslint-disable-next-line
-  }, []);
+		return () => (isMounted = false);
+		// eslint-disable-next-line
+	}, []);
 
-  useEffect(() => {
-    // console.log(`isLoading: ${isLoading}`);
-    // console.log(`aT: ${JSON.stringify(auth?.accessToken)}`);
-    // eslint-disable-next-line
-  }, [isLoading]);
+	useEffect(() => {
+		// console.log(`isLoading: ${isLoading}`);
+		// console.log(`aT: ${JSON.stringify(auth?.accessToken)}`);
+		// eslint-disable-next-line
+	}, [isLoading]);
 
-  return (
-    <>
-      {isLoading ? <p className="text-neutral-300">Loading...</p> : <Outlet />}
-    </>
-  );
+	return (
+		<>
+			{isLoading ? (
+				<p className="text-neutral-300">
+					Loading... This may take up to 30 seconds
+				</p>
+			) : (
+				<Outlet />
+			)}
+		</>
+	);
 };
 
 export default PersistLogin;
